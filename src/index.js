b02f0643e5aba06bf17668ef990e866331ffec55
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-09-10 11:33:03 -0700

    add variable support

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import { print } from 'graphql-tag/printer';
 import { mapValues, get } from 'lodash';
 import rp from 'request-promise';
 
-//type RequestType = {
-//  debugName: string,
-//  query: Object,
-//  variables: Object,
-//  operationName: string,
-//};
-
-class RedEye {
+export default class RedEye {
   constructor(params) {
     this.origin = get(params, 'origin');
   }
-
-  generateQuery(query) {
+  generateQuery(query, variables) {
     return mapValues({
       query,
+      variables,
     }, (val, key) => {
       return key === 'query' ? print(val) : val;
     });
@@ -31,45 +24,6 @@ class RedEye {
       json: true // Automatically stringifies the body to JSON
     };
 
-    return rp(options)
-      .then(function (parsedBody) {
-        // POST succeeded...
-        console.log(get(parsedBody, 'data'));
-      })
-      .catch(function (err) {
-        // POST failed...
-        console.log(err);
-      });
+    return rp(options);
   }
 }
-
-
-/***
- * TESTING
- * @type {RedEye}
- */
-const QueryManager = new RedEye({
-  origin: 'http://localhost:8000/graphql'
-});
-
-const allCountries = gql`
-  {
-    allCountries {
-      name
-    }
-  }
-`;
-
-const allCountriesQuery = QueryManager.generateQuery(allCountries);
-console.log(allCountriesQuery);
-QueryManager.sendQuery(allCountriesQuery);
-
-const testMutation = gql`
-    mutation Test {
-      test: testCountryMutation(yo: "123")
-    }
-`;
-const countryMutation = QueryManager.generateQuery(testMutation);
-console.log(countryMutation);
-QueryManager.sendQuery(countryMutation);
-
